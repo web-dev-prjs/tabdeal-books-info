7be21d3d62feabe9a983da9ba758c38c0499c939
omid <web.dev.prjs@gmail.com>
2024-07-02 23:59:59 +0330

    Improve: Add ability to create the custom-table in database.
    [Ticket: 20240703#110]

--- includes/Core/Activate.php
+++ includes/Core/Activate.php
@@ -8,13 +8,15 @@
 
 namespace TBI\Core;
 
+use TBI\Core\Factories\CustomTable;
+
 /**
  * Activate class.
  *
  * @since 1.0.0
  */
 final class Activate {
-
+	
 	/**
 	 * Runs in the activation time.
 	 *
@@ -22,12 +24,28 @@ final class Activate {
 	 * @since 1.0.0
 	 */
 	public static function activate(): void {
-
+		
 		/*
 		 |-----------------------------------------------------------
 		 | Removes rewrite rules and then recreate rewrite rules.
 		 |-----------------------------------------------------------
 		 */
 		flush_rewrite_rules();
+		
+		/*
+		 |--------------------------------
+		 | Creates books-info table.
+		 | @since 1.1.0
+		 |--------------------------------
+		 */
+		CustomTable::create(
+			'books_info',
+			'
+			ID bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+	        ISBN tinytext NOT NULL,
+	        post_id bigint UNSIGNED NOT NULL,
+	        PRIMARY KEY  (ID)
+	        '
+		);
 	}
 }
