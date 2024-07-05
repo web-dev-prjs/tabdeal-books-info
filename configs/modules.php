7b89827afb0965c672e12964d9d54d6a20308886
omid <web.dev.prjs@gmail.com>
2024-07-06 00:49:05 +0330

    Improve: Add the `LikeDislike` feature.
    [Ticket: 20240706#140]

--- configs/modules.php
+++ configs/modules.php
@@ -3,28 +3,30 @@
 /**
  * Returns an array of modules and services.
  *
- * @since 1.1.0
+ * @since 1.0.0
  */
+
 return array(
 	/*
 	 |-------------------------------------------------------------------------
 	 | Base configs, to setting up this plugin.
 	 |-------------------------------------------------------------------------
 	 */
-	\TBI\Core\Enqueue::class,
-	\TBI\Core\ActionLink::class,
+	\TBI\Core\Enqueue::class, // @since 1.0.0
+	\TBI\Core\ActionLink::class, // @since 1.0.1
 	
 	/*
 	 |-------------------------------------------------------------------------
 	 | Admin configs, to implement admin dashboard of plugin.
 	 |-------------------------------------------------------------------------
 	 */
-	\TBI\Core\Dashboard::class,
+	\TBI\Core\Dashboard::class, // @since 1.0.0
 	
 	/*
 	 |-------------------------------------------------------------------------
 	 | Services configs, to implement all services of plugin.
 	 |-------------------------------------------------------------------------
 	 */
-	\TBI\Services\BookCPT::class
+	\TBI\Services\BookCPT::class, // @since 1.2.0
+	\TBI\Services\LikeDislike::class // @since 1.4.0
 );
